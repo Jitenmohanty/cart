93ac007119e8b94a18efd37f0845633492005d4a
Jiten Kumar Mohanty <jitenmohantyaz@gmail.com>
2023-05-10 13:39:18 +0530

    Added states using objects

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,32 +1,45 @@
-import React from 'react'
+import React from "react";
 
-class CartItem extends React.Component{
-    render(){
-        return(
-            <div className='cart-item'>
-                <div className="left-block">
-                    <img style={style.img}/>
-                </div>
-                <div className="right-block">
-                    <div style={{fontSize:25}}>Phone</div>
-                    <div style={{fontSize:25}}>999</div>
-                    <div style={{fontSize:25}}>Qty:1</div>
-                    <div className="cart-item-actions">
-                        {/* Buttons */}
-                    </div>
-                </div>
-            </div>
-        )
+class CartItem extends React.Component {
+    constructor(){
+        super();
+        this.state={
+            price:999,
+            title:'Phone',
+            qty:1,
+            img:''
+        }
     }
+  render() {
+    const {price,title,qty} = this.state;
+    return (
+      <div className="cart-item">
+        <div className="left-block">
+          <img style={style.img} />
+        </div>
+        <div className="right-block">
+          <div style={{ fontSize: 25 }}>{title}</div>
+          <div style={{ color:'#777' }}>Rs {price}</div>
+          <div style={{ color:'#777'}}>Qty:{qty}</div>
+          <div className="cart-item-actions">
+            {/* Buttons */}
+            <img src="https://cdn-icons-png.flaticon.com/128/2569/2569198.png" className="action-icons" alt="Increse" />
+            <img src="https://cdn-icons-png.flaticon.com/128/1828/1828817.png" className="action-icons" alt="Decrese" />
+            <img src="https://cdn-icons-png.flaticon.com/128/3096/3096687.png" className="action-icons" alt="Delete" />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 const style = {
-    img:{
-        height:110,
-        width:110,
-        borderRadius:4,
-        backgroundColor:'red'
-    }
-}
+  img: {
+    height: 110,
+    width: 110,
+    borderRadius: 4,
+    backgroundColor: "grey",
+  },
+};
 
 export default CartItem;
